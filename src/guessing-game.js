8a5af3e113c7132a9e94030033add317e2ab4e71
Stas Gatilov <stasgat12@gmail.com>
2022-08-16 17:50:27 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    let guessNumber = Math.round((this.max + this.min) / 2);
+    return guessNumber;
+  }
 
-    guess() {
+  lower() {
+    return (this.max = Math.round((this.max + this.min) / 2));
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    return (this.min = Math.round((this.max + this.min) / 2));
+  }
 }
 
 module.exports = GuessingGame;
